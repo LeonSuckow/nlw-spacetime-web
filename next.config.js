e09ec007f07277d79a28b95ed94e49d3cffce4bb
LeonSuckow <leonlucassuckow@hotmail.com>
2023-08-28 18:12:59 -0300

    add domain

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // output: 'export',
   images: {
     domains: [
       'images.unsplash.com',
       'avatars.githubusercontent.com',
       '192.168.1.3',
+      'nlw-spacetime-web-production.up.railway.app/',
     ],
   },
 }
