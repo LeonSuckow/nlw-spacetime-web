0cddd8dfd41d21819fd9b1a2b87b60390b78d8da
LeonSuckow <leonlucassuckow@hotmail.com>
2023-08-28 20:14:23 -0300

    testing request url

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -6,7 +6,7 @@ export async function GET(request: NextRequest) {
 
   const code = searchParams.get('code')
   console.log(code)
-
+  console.log(new URL('/', request.url))
   const redirectTo = request.cookies.get('redirectTo')?.value
   try {
     const registerResponse = await api.post('/register', {
