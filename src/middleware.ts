3192045d070f876c2451acc05f6165bbf3515db5
LeonSuckow <leonlucassuckow@hotmail.com>
2023-08-29 20:36:54 -0300

    try solution host NextRequest

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,16 +3,21 @@ import { NextRequest, NextResponse } from 'next/server'
 const signInURL = `https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`
 
 export function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname.startsWith('/memories')) {
-    const token = request.cookies.get('token')?.value
-    if (!token) {
-      return NextResponse.redirect(signInURL, {
-        headers: {
-          'Set-Cookie': `redirectTo=${request.url}; Path=/; HttpOnly; max-age=20;`,
-        },
-      })
-    }
+  const token = request.cookies.get('token')?.value
+  let { nextUrl, headers, url } = request
+  nextUrl.host = headers.get('Host') ?? nextUrl.host
+  url = nextUrl.href
+  if (!token) {
+    return NextResponse.redirect(signInURL, {
+      headers: {
+        'Set-Cookie': `redirectTo=${url}; Path=/; HttpOnly; max-age=20;`,
+      },
+    })
   }
 
   return NextResponse.next()
+}
+
+export const config = {
+  matcher: '/memories/:path*',
 }
